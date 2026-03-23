d039fd6fefba54318dda2a898f2f623137a048b6
Krishani03 <krishanik932@gmail.com>
2026-03-23 13:13:38 +0530

    complete booking system

--- src/main/java/com/example/smarttourism/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.smarttourism.entity;
-
-public enum Role {
-    TOURIST,
-    GUIDE,
-    ADMIN
-}
--- src/main/java/com/example/smarttourism/repository/TourPackageRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.smarttourism.repository;
-
-import com.example.smarttourism.entity.TourPackage;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TourPackageRepository extends JpaRepository<TourPackage, Long> {
-}
